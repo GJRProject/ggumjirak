6426f0201278f3ed265c909a29980d4cb9f0764f
techsoup5team <74398140+techsoup5team@users.noreply.github.com>
2020-11-16 17:13:56 +0900

    ggumjirak

--- project/gjr/wsgi.py
+++ project/gjr/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for team5 project.
+WSGI config for gjr project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "team5.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gjr.settings")
 
 application = get_wsgi_application()
