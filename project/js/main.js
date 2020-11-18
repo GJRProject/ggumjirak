fef82c76df237dfd21594e88d38dc43e4bab92bc
wkdms3509 <chlwkdms7@naver.com>
2020-11-18 19:11:06 +0900

    기본 홈화면 네비 추가
    기본 홈화면 네비 추가(html, css, reset(초기화 css), js)

---
+++ project/js/main.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.navbar_toogleBtn');
+const menu = document.querySelector('.navbar_menu');
+const links = document.querySelector('.navbar_links');
+
+toogleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  links.classList.toggle('active');
+});
