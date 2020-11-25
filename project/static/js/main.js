1b941e876b6b535d9cfb2a23b59327bcf694ae55
wkdms3509 <chlwkdms7@naver.com>
2020-11-25 16:48:32 +0900

    html 수정
    html 수정

--- project/static/js/main.js
+++ project/static/js/main.js
@@ -2,10 +2,12 @@ const toggleBtn = document.querySelector('.navbar_toogleBtn');
 const menu = document.querySelector('.navbar_menu');
 const links = document.querySelector('.navbar_links');
 
+/*
 toogleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
   links.classList.toggle('active');
 });
+*/
 
 $('.carousel').carousel({
   interval: 2000
