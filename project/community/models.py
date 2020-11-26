79a8ea8ee913c84ecfe952f29463da4809226a11
techsoup5team <74398140+techsoup5team@users.noreply.github.com>
2020-11-26 14:41:47 +0900

    board-img

--- project/community/models.py
+++ project/community/models.py
@@ -4,9 +4,8 @@ from django.db import models
 # 게시글(Post)엔 제목(postname), 내용(contents)이 존재합니다
 class Post(models.Model):
     postname = models.CharField(max_length=50)
-    mainphoto = models.ImageField(blank=True, null=True)
     contents = models.TextField()
-    
+    mainphoto = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.postname
