55f08c39aafff2444d28883b7ce72866585fa353
suri <suri@socreative.org>
2020-11-26 15:02:14 +0900

    Revert "board-img"
    This reverts commit 79a8ea8ee913c84ecfe952f29463da4809226a11.

--- project/gjr/urls.py
+++ project/gjr/urls.py
@@ -1,26 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from community.views import home, blog, posting
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('community.urls')),
 ]
-
-# 이미지를 업로드하자+
-
-from django.conf.urls.static import static
-from django.conf import settings
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # 웹사이트의 첫화면은 index 페이지이다 + URL이름은 index이다
-    path('', home, name='home'),
-    # URL:80/blog에 접속하면 blog 페이지 + URL이름은 blog이다
-    path('blog/', blog, name='blog'),
-    # URL:80/blog/숫자로 접속하면 게시글-세부페이지(posting)
-    path('blog/<int:pk>/', posting, name='posting'),
-]
-
-# 이미지 URL 설정
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
