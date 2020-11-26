9aa4e3c6cc46bbe3a2b6fd52930c3f3ed7ba5e16
suri <suri@socreative.org>
2020-11-26 18:31:12 +0900

    for temp deploy

--- project/community/urls.py
+++ project/community/urls.py
@@ -5,8 +5,6 @@ urlpatterns = [
     path('', views.home),
     path('schedule/', views.schedule),
     path('donation/', views.donation),
-    path('beta/', views.beta),
-    path('gamma/', views.gamma),
     path('blog/', views.blog),
     path('blog/<int:pk>/',views.posting, name='posting'),
     path('contact/', views.contact),
