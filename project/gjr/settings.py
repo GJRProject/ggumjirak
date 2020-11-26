9844e05ac196d6ad1c6e686ec6adad6bd921e87d
suri <suri@socreative.org>
2020-11-26 17:23:40 +0900

    temp ALLOWED_HOSTS update

--- project/gjr/settings.py
+++ project/gjr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')ek(dvg1j(v%!s2z!cc1-b0$0d^puctxf4+rgx304ttbi2q43_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ggum.socreative.org']
 
 
 # Application definition
