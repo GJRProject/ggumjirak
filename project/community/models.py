bc3b9c6733727e0dd5e87640ab822c4425e17598
techsoup5team <74398140+techsoup5team@users.noreply.github.com>
2020-11-27 17:02:49 +0900

    날짜 글번호

--- project/community/models.py
+++ project/community/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 # 게시글(Post)엔 제목(postname), 내용(contents)이 존재합니다
@@ -15,8 +16,8 @@ class Notice(models.Model):
     postname = models.CharField(max_length=50)
     mainphoto = models.ImageField(blank=True, null=True)
     contents = models.TextField()
-    
-
+    pub_date = models.DateTimeField(default=timezone.now)
+        
     def __str__(self):
         return self.postname
 
